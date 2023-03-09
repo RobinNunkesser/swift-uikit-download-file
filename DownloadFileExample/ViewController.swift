0e7ff5cfdb39c150e45f4266bbf820f154d243fb
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-09 14:01:49 +0100

    Initial example

--- DownloadFileExample/ViewController.swift
+++ DownloadFileExample/ViewController.swift
@@ -7,11 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    let percentFormatter : NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        return formatter
+    }()
+    
+    private lazy var urlSession = URLSession(configuration: .default,
+                                             delegate: self,
+                                             delegateQueue: nil)
+    var downloadTask : URLSessionDownloadTask?
 
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "http://ipv4.download.thinkbroadband.com/50MB.zip")!
+        
+        startDownload(url: url)
+    }
+    
+    private func startDownload(url: URL) {
+        let downloadTask = urlSession.downloadTask(with: url)
+        downloadTask.resume()
+        self.downloadTask = downloadTask
+    }
+    
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64,
+                    totalBytesWritten: Int64,
+                    totalBytesExpectedToWrite: Int64) {
+        if downloadTask == self.downloadTask {
+            let calculatedProgress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+            DispatchQueue.main.async {
+                self.progressLabel.text = self.percentFormatter.string(from:
+                                                                        NSNumber(value: calculatedProgress))
+            }
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        debugPrint(location)
     }
 
 
